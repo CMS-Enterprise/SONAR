e428044075a54756da8d3e0c037947f5f7bb9840
btakushi <blaise.takushi@cms.hhs.gov>
2022-11-07 14:57:54 -1000

    Completed integration with service hierarchy

--- sonar-agent/Program.cs
+++ sonar-agent/Program.cs
@@ -186,81 +186,66 @@ internal static class Program {
       var tenant = await client.GetTenantAsync(config.Environment, config.Tenant, token);
       Console.WriteLine($"Service Count: {tenant.Services.Count}");
 
-      // hardcode healthcheck dictionary for now
-      var healthChecks = new List<dynamic>();
-      dynamic healthCheck1 = new ExpandoObject();
-      dynamic healthCheck2 = new ExpandoObject();
-      dynamic condition1 = new ExpandoObject();
-      dynamic condition2 = new ExpandoObject();
-      dynamic condition3 = new ExpandoObject();
-      dynamic condition4 = new ExpandoObject();
-      dynamic conditionList1 = new List<dynamic>();
-      dynamic conditionList2 = new List<dynamic>();
-
-      condition1.status = HealthStatus.Online;
-      condition1.op = HealthOperator.GreaterThan;
-      condition1.threshold = 3.12;
-
-      condition2.status = HealthStatus.Online;
-      condition2.op = HealthOperator.GreaterThan;
-      condition2.threshold = 3.12;
-
-      condition3.status = HealthStatus.Degraded;
-      condition3.op = HealthOperator.GreaterThan;
-      condition3.threshold = 3.11;
-
-      condition4.status = HealthStatus.Offline;
-      condition4.op = HealthOperator.GreaterThan;
-      condition4.threshold = 3.10;
-
-      conditionList1.Add(condition1);
-      conditionList1.Add(condition2);
-      conditionList2.Add(condition3);
-      conditionList2.Add(condition4);
-
-      healthCheck1.conditions = conditionList1;
-      healthCheck1.name = "health-check-1";
-      healthCheck2.conditions = conditionList2;
-      healthCheck2.name = "health-check-2";
-
-      healthChecks.Add(healthCheck1);
-      healthChecks.Add(healthCheck2);
-
       foreach (var service in tenant.Services) {
         Console.WriteLine($"SONAR_AGENT: Evaluating health for Service: {service.Name}.");
         Int32? aggStatus = null;
+        // Get service's health checks here
+        var healthChecks = service.HealthChecks;
+
+        if (healthChecks == null) {
+          Console.WriteLine("No Health Checks associated with this service.");
+          continue;
+        }
+
         foreach (var healthCheck in healthChecks) {
           var currCheck = HealthStatus.Online;
+          // Set start and end for date range, Get Prometheus samples
+          var end = DateTime.UtcNow;
+          var start = end.Subtract(healthCheck.Definition.Duration);
+          var qrResult = await promClient.QueryRangeAsync(healthCheck.Definition.Expression, start,
+            end, TimeSpan.FromHours(1), null, token);
 
-          // Get Prometheus samples
-          // var qResult = await promClient.QueryAsync("test", Convert.ToDateTime("2022-11-03T18:32:00Z"),
-          //   TimeSpan.FromMilliseconds(1000), token);
-          // var sample = qResult.Data.Result[0].Value;
-          var qrResult = await promClient.QueryRangeAsync("test", Convert.ToDateTime("2022-11-03T18:32:00Z"),
-            Convert.ToDateTime("2022-11-03T18:32:10Z"), TimeSpan.FromSeconds(1), null, token);
+          // Error handling
+          if (qrResult.Data == null) {
+            // No data, bad request
+            Console.Error.WriteLine($"Prometheus returned nothing for health check: {healthCheck.Name}");
+            continue;
+          }
+
+          if (qrResult.Data.Result.Count > 1) {
+            // Bad config, multiple time series returned
+            Console.Error.WriteLine($"Invalid Prometheus configuration, multiple time series returned for health check: {healthCheck.Name}");
+            continue;
+          }
+
+          if (qrResult.Data.Result.Count == 0 || qrResult.Data.Result[0].Values == null ||
+              qrResult.Data.Result[0].Values!.Count == 0) {
+            // No samples
+            Console.Error.WriteLine($"Prometheus returned no samples for health check: {healthCheck.Name}");
+            continue;
+          }
 
           var samples = qrResult.Data.Result[0].Values;
 
-          foreach (var condition in healthCheck.conditions) {
+          Console.WriteLine($"Evaluating PromQL Expression: {healthCheck.Definition.Expression} ");
+
+          foreach (var condition in healthCheck.Definition.Conditions) {
             // Determine which comparison to execute
             // Evaluate all PromQL samples
-            var evaluation = EvaluateSamples(condition.op, samples, condition.threshold);
+            var evaluation = EvaluateSamples(condition.HealthOperator, samples, condition.Threshold);
 
             // If evaluation is true, set the current check to the condition's status
             // and output to Stdout
             if (evaluation) {
-              currCheck = condition.status;
-              // Console.WriteLine($"Sample {sample?.Value} was {(HealthOperator)condition.op} {condition.threshold}");
-              Console.WriteLine(
-                $"Service: {config.Environment}/{config.Tenant}/{service.Name}; Check: {healthCheck.name}; Status: {currCheck}");
+              currCheck = condition.HealthStatus;
+              Console.WriteLine($"Service: {config.Environment}/{config.Tenant}/{service.Name}; Check: {healthCheck.Name}; Status: {currCheck}");
               break;
             }
           }
 
           if (currCheck == HealthStatus.Online) {
             Console.WriteLine($"No conditions were met... service is online");
-            Console.WriteLine(
-              $"Service: {config.Environment}/{config.Tenant}/{service.Name}; Check: {healthCheck.name}; Status: {currCheck}");
+            Console.WriteLine($"Service: {config.Environment}/{config.Tenant}/{service.Name}; Check: {healthCheck.Name}; Status: {currCheck}");
           }
 
           if ((Int32)currCheck > (aggStatus ?? 0)) {
--- sonar-agent/SonarClient.cs
+++ sonar-agent/SonarClient.cs
@@ -1,9 +1,13 @@
 using System.Text.Json;
+using System.Text.Json.Serialization;
+using Cms.BatCave.Sonar.Json;
 
 namespace Cms.BatCave.Sonar.Agent;
 
 public partial class SonarClient {
   partial void UpdateJsonSerializerSettings(JsonSerializerOptions settings) {
     settings.PropertyNameCaseInsensitive = true;
+    settings.Converters.Add(new JsonStringEnumConverter());
+    settings.Converters.Add(new ArrayTupleConverterFactory());
   }
 }
