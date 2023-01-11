c0a5182805b5bbde15ed0aba820c0b39b4af72e6
Kevin Ly <kevin.ly@cms.hhs.gov>
2023-01-10 16:42:44 -0800

    Added default parameters for appsettings location, and cleanup

--- sonar-agent/Options/InitSettings.cs
+++ sonar-agent/Options/InitSettings.cs
@@ -1,18 +1,14 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using CommandLine;
 
 namespace Cms.BatCave.Sonar.Agent.Options;
 
 public class InitSettings {
-  [Value(0,  HelpText = "AppSetting file Location.")]
+  //[Value(0,  HelpText = "AppSetting file Location.")]
+  [Option("appsetting-location", HelpText = "AppSetting file Location.", Default = ".")]
   public String AppSettingLocation { get; set; }
 
-  [Value(1, Min = 1, HelpText = "Service Config Files.")]
+  [Value(0, Min = 1, HelpText = "Service Config Files.")]
   public IEnumerable<String> ServiceConfigFiles { get; set; }
 }
-
-
-
-
--- sonar-agent/Program.cs
+++ sonar-agent/Program.cs
@@ -59,56 +59,56 @@ internal static class Program {
     // API Configuration
     var builder = new ConfigurationBuilder()
       .SetBasePath(Directory.GetCurrentDirectory())
-      .AddJsonFile(opts.AppSettingLocation+"/"+"appsettings.json", false, true)
-      .AddJsonFile(opts.AppSettingLocation+"/"+$"appsettings.{Environment.GetEnvironmentVariable("ENVIRONMENT")}.json", true, true)
+      .AddJsonFile("appsettings.json", true, true)
+      .AddJsonFile(Path.Combine(opts.AppSettingLocation, "appsettings.json" ), true, true)
+      .AddJsonFile(Path.Combine(opts.AppSettingLocation, $"appsettings.{Environment.GetEnvironmentVariable("ENVIRONMENT")}.json" ), true, true)
       .AddEnvironmentVariables();
 
-            IConfigurationRoot configuration = builder.Build();
-        var apiConfig = configuration.GetSection("ApiConfig").BindCtor<ApiConfiguration>();
-        var promConfig = configuration.GetSection("Prometheus").BindCtor<PrometheusConfiguration>();
-        var lokiConfig = configuration.GetSection("Loki").BindCtor<LokiConfiguration>();
+    IConfigurationRoot configuration = builder.Build();
+    var apiConfig = configuration.GetSection("ApiConfig").BindCtor<ApiConfiguration>();
+    var promConfig = configuration.GetSection("Prometheus").BindCtor<PrometheusConfiguration>();
+    var lokiConfig = configuration.GetSection("Loki").BindCtor<LokiConfiguration>();
 
-        // Create cancellation source, token, new task
-        var source = new CancellationTokenSource();
-        CancellationToken token = source.Token;
+    // Create cancellation source, token, new task
+    var source = new CancellationTokenSource();
+    CancellationToken token = source.Token;
 
-        // Event handler for SIGINT
-        // Traps SIGINT to perform necessary cleanup
-        Console.CancelKeyPress += delegate {
-          Console.WriteLine("\nSIGINT received, begin cleanup...");
-          source.Cancel();
-        };
+    // Event handler for SIGINT
+    // Traps SIGINT to perform necessary cleanup
+    Console.CancelKeyPress += delegate {
+      Console.WriteLine("\nSIGINT received, begin cleanup...");
+      source.Cancel();
+    };
 
-        try {
-          // Load and merge configs
-          var servicesHierarchy = await ConfigurationHelper.LoadAndValidateJsonServiceConfig(opts.ServiceConfigFiles.ToArray(), token);
-          // Configure service hierarchy
-          Console.WriteLine("Configuring services....");
-          await ConfigurationHelper.ConfigureServices(configuration, apiConfig, servicesHierarchy, token);
-          // Hard coded 10 second interval
-          var interval = TimeSpan.FromSeconds(10);
-          Console.WriteLine("Initializing SONAR Agent...");
-          // Run task that calls Health Check function
-          var task = Task.Run(
-            async delegate {
-              await HealthCheckHelper.RunScheduledHealthCheck(interval, configuration, apiConfig, promConfig,
-                lokiConfig, token);
-            }, token);
-          await task;
-        }
-        catch (IndexOutOfRangeException) {
-          Console.Error.WriteLine("First command line argument must be service configuration file path.");
-        }
-        catch (OperationCanceledException e) {
-          Console.Error.WriteLine(e.Message);
-          Console.Error.WriteLine($"{nameof(OperationCanceledException)} thrown with message: {e.Message}");
-          // Additional cleanup goes here
-        }
-        finally {
-          source.Dispose();
-        }
+    try {
+      // Load and merge configs
+      var servicesHierarchy = await ConfigurationHelper.LoadAndValidateJsonServiceConfig(opts.ServiceConfigFiles.ToArray(), token);
+      // Configure service hierarchy
+      Console.WriteLine("Configuring services....");
+      await ConfigurationHelper.ConfigureServices(configuration, apiConfig, servicesHierarchy, token);
+      // Hard coded 10 second interval
+      var interval = TimeSpan.FromSeconds(10);
+      Console.WriteLine("Initializing SONAR Agent...");
+      // Run task that calls Health Check function
+      var task = Task.Run(
+        async delegate {
+          await HealthCheckHelper.RunScheduledHealthCheck(interval, configuration, apiConfig, promConfig,
+            lokiConfig, token);
+        }, token);
+      await task;
+    }
+    catch (IndexOutOfRangeException) {
+      Console.Error.WriteLine("First command line argument must be service configuration file path.");
+    }
+    catch (OperationCanceledException e) {
+      Console.Error.WriteLine(e.Message);
+      Console.Error.WriteLine($"{nameof(OperationCanceledException)} thrown with message: {e.Message}");
+      // Additional cleanup goes here
+    }
+    finally {
+      source.Dispose();
+    }
 
     return 0;
   }
 }
-
