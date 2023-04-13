8df34de0930f758e1f9c2904ed97eb2bc04c29a2
Stephen Brey <stephen.brey@cms.hhs.gov>
2023-04-12 19:34:49 -0600

    Updates log message

--- sonar-api/Controllers/HealthCheckDataController.cs
+++ sonar-api/Controllers/HealthCheckDataController.cs
@@ -80,8 +80,8 @@ public class HealthCheckDataController : ControllerBase {
     }
 
     this._logger.LogDebug(
-      message: "Received health check sample data for " +
-       $"environment = \"{environment}\", tenant = \"{tenant}\", service = \"{service}\": {data}");
+      message: $"Received {data.TotalSamples} samples from {data.TotalHealthChecks} health checks for" +
+       $" environment = \"{environment}\", tenant = \"{tenant}\", service = \"{service}\": {data}");
 
 
     var recordedData = await this._prometheusService.WriteHealthCheckDataAsync(
