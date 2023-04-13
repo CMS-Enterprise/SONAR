45a0903a2d3140ecd8d2f330a6dbfefc309b7226
Stephen Brey <stephen.brey@cms.hhs.gov>
2023-04-13 09:36:32 -0600

    Updates quoting in ToString override

--- sonar-core/Models/ServiceHealthData.cs
+++ sonar-core/Models/ServiceHealthData.cs
@@ -14,7 +14,7 @@ public record ServiceHealthData(
     StringBuilder sb = new();
     sb.Append($"{nameof(HealthCheckSamples)} {{ ");
     foreach (var (healthCheck, samples) in this.HealthCheckSamples) {
-      sb.Append($"\n  [\"{healthCheck}\"] = {{");
+      sb.Append($"\n  ['{healthCheck}'] = {{");
       foreach (var (timestamp, value) in samples ?? ImmutableArray<(DateTime Timestamp, Double Value)>.Empty) {
         sb.Append($"\n    ({timestamp.MillisSinceUnixEpoch()}, {value}),");
       }
