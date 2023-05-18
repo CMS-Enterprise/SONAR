e48e7e406ace022719b1a41231fc3134f613b633
Teresa Tran <teresabernadette.tran@cms.hhs.gov>
2023-05-18 11:14:33 -0700

    BATAPI-250 added health status badges component

---
+++ sonar-ui/src/components/Badges/HealthStatusBadge.Style.ts
@@ -0,0 +1,30 @@
+import { css, Theme } from '@emotion/react';
+import { HealthStatus } from '../../api/data-contracts';
+import { getStatusColors } from '../../helpers/StyleHelper';
+
+export function getBadgeTextColor(status: HealthStatus | undefined) {
+  let color;
+  switch (status) {
+    case HealthStatus.Online:
+    case HealthStatus.Offline:
+      color = '#FFF';
+      break;
+    case HealthStatus.Unknown:
+    case HealthStatus.AtRisk:
+    case HealthStatus.Degraded:
+      color = '#262626';
+      break;
+    default:
+      color = '#262626';
+  }
+  return color;
+}
+
+export function badgeStyle(theme: Theme, status: HealthStatus | undefined) {
+  return css({
+    color: getBadgeTextColor(status),
+    textAlign: 'center',
+    width: '70px',
+    "--badge__background-color": getStatusColors(theme, status)
+  });
+}
---
+++ sonar-ui/src/components/Badges/HealthStatusBadge.tsx
@@ -0,0 +1,21 @@
+import { Badge } from '@cmsgov/design-system';
+import { css, Theme } from '@emotion/react';
+import React from 'react';
+import { HealthStatus } from '../../api/data-contracts';
+import { badgeStyle } from './HealthStatusBadge.Style';
+
+const HealthStatusBadge: React.FC<{
+  theme: Theme,
+  status: HealthStatus | undefined
+}> =
+  ({ theme, status}) => {
+    return (
+      < Badge
+        children={HealthStatus}
+        size="big"
+        css={badgeStyle(theme, status)}
+      />
+    );
+  };
+
+export default HealthStatusBadge;
