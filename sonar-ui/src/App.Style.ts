d1ec46915722cc0f27ae438b5b1b403c7010d57c
Teresa Tran <teresabernadette.tran@cms.hhs.gov>
2023-05-22 13:52:13 -0700

    BATAPI-250: added accordion container style

--- sonar-ui/src/App.Style.ts
+++ sonar-ui/src/App.Style.ts
@@ -16,6 +16,9 @@ export function mainStyle(theme: Theme) {
     '--link__color': theme.textColor,
     '--link__color--active': theme.textColor,
     '--link__color--hover': theme.accentColor,
-    '--link__color--visited': theme.textColor
+    '--link__color--visited': theme.textColor,
+    '.accordionContainerStyles': {
+      border: '1px #a6a6a6',
+      borderRadius: '40px'}
   });
 }
