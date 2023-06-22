f6db5dbbd6974f7984f2d66bb75a3c1178a6c79e
btakushi <blaise.takushi@cms.hhs.gov>
2023-06-22 09:18:25 -1000

    fix undefined status bug in health status drawer

--- sonar-ui/src/components/Services/HealthStatus/HealthStatusDrawer.tsx
+++ sonar-ui/src/components/Services/HealthStatus/HealthStatusDrawer.tsx
@@ -1,20 +1,17 @@
-import React, { useContext, useState } from 'react';
 import { Drawer } from '@cmsgov/design-system';
-import { ServiceOverviewContext } from '../ServiceOverviewContext';
-import HealthStatusBadge from 'components/Badges/HealthStatusBadge';
 import { useTheme } from '@emotion/react';
 import { HealthCheckType, HealthStatus } from 'api/data-contracts';
-import HealthMetricThresholds from './HealthMetricThresholds';
-import { createSonarClient } from 'helpers/ApiHelper';
-import { useQuery } from 'react-query';
-import HealthStatusDataTimeSeriesChart from './HealthStatusDataTimeSeriesChart';
-import { IHealthCheckDefinition } from 'types';
-import HealthStatusDataTable from './HealthStatusDataTable';
-import {
-  getDrawerSectionHeaderStyle,
-  getDrawerStyle
-} from '../Drawer.Style';
 import { DynamicTextFontStyle } from 'App.Style'
+import HealthStatusBadge from 'components/Badges/HealthStatusBadge';
+import { createSonarClient } from 'helpers/ApiHelper';
+import React, { useContext, useState } from 'react';
+import { useQuery } from 'react-query';
+import { IHealthCheckDefinition } from 'types';
+import { getDrawerSectionHeaderStyle, getDrawerStyle } from '../Drawer.Style';
+import { ServiceOverviewContext } from '../ServiceOverviewContext';
+import HealthMetricThresholds from './HealthMetricThresholds';
+import HealthStatusDataTable from './HealthStatusDataTable';
+import HealthStatusDataTimeSeriesChart from './HealthStatusDataTimeSeriesChart';
 
 const HealthStatusDrawer: React.FC<{
   onCloseClick: () => void
@@ -26,7 +23,9 @@ const HealthStatusDrawer: React.FC<{
 
   const serviceConfiguration = context.serviceConfiguration;
   const healthCheck = context.selectedHealthCheck!;
-  const healthCheckStatus = context.serviceHierarchyHealth.healthChecks![healthCheck.name][1] as HealthStatus;
+  const healthCheckStatus = context.serviceHierarchyHealth.healthChecks![healthCheck.name] ?
+    context.serviceHierarchyHealth.healthChecks![healthCheck.name][1] as HealthStatus :
+    null;
   const healthCheckDefinition = healthCheck.definition as IHealthCheckDefinition;
   const isMetricHealthCheck = [HealthCheckType.LokiMetric, HealthCheckType.PrometheusMetric].includes(healthCheck.type);
   const drawerHeading = `Health Checks`;
@@ -46,23 +45,25 @@ const HealthStatusDrawer: React.FC<{
 
   return (
     <Drawer css={getDrawerStyle} heading={drawerHeading} headingLevel="3" onCloseClick={onCloseClick}>
-      <>
-        <div css={DynamicTextFontStyle}>
-          <b>{healthCheck.name}&nbsp;</b>
-          <HealthStatusBadge theme={theme} status={healthCheckStatus} />
-        </div>
+      {healthCheckStatus && (
+        <>
+          <div css={DynamicTextFontStyle}>
+            <b>{healthCheck.name}&nbsp;</b>
+            <HealthStatusBadge theme={theme} status={healthCheckStatus} />
+          </div>
 
-        <h4 css={getDrawerSectionHeaderStyle}>Health Conditions&nbsp;-&nbsp;{healthCheck.type}</h4>
-        <HealthMetricThresholds service={serviceConfiguration} healthCheck={healthCheck} healthCheckStatus={healthCheckStatus} />
+          <h4 css={getDrawerSectionHeaderStyle}>Health Conditions&nbsp;-&nbsp;{healthCheck.type}</h4>
+          <HealthMetricThresholds service={serviceConfiguration} healthCheck={healthCheck} healthCheckStatus={healthCheckStatus} />
 
-        { isMetricHealthCheck && (
-          <>
-            <h4 css={getDrawerSectionHeaderStyle}>Health Check Metrics</h4>
-            <HealthStatusDataTimeSeriesChart key={`${healthCheck.name}-ts`} svcDefinitions={healthCheckDefinition} healthCheckName={healthCheck.name} timeSeriesData={timeSeriesData} />
-            <HealthStatusDataTable key={`${healthCheck.name}-dt`} healthCheckName={healthCheck.name} timeSeriesData={timeSeriesData} />
-          </>
-        )}
-      </>
+          { isMetricHealthCheck && (
+            <>
+              <h4 css={getDrawerSectionHeaderStyle}>Health Check Metrics</h4>
+              <HealthStatusDataTimeSeriesChart key={`${healthCheck.name}-ts`} svcDefinitions={healthCheckDefinition} healthCheckName={healthCheck.name} timeSeriesData={timeSeriesData} />
+              <HealthStatusDataTable key={`${healthCheck.name}-dt`} healthCheckName={healthCheck.name} timeSeriesData={timeSeriesData} />
+            </>
+          )}
+        </>
+      )}
     </Drawer>
   );
 }
