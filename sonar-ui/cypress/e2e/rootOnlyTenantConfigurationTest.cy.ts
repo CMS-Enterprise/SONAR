f2544f187067dfd3a60dc9c18cfc68c4531c436d
Teresa Tran <teresabernadette.tran@cms.hhs.gov>
2023-06-23 12:17:32 -0700

    BATAPI-278: fixed lint issue

--- sonar-ui/cypress/e2e/rootOnlyTenantConfigurationTest.cy.ts
+++ sonar-ui/cypress/e2e/rootOnlyTenantConfigurationTest.cy.ts
@@ -1,5 +1,3 @@
-
-const rootOnlyConfig = require('../fixtures/testRootConfig')
 const environmentName = 'foo'
 const tenantName = 'bar'
 const configRequestUrl = 'localhost:8081/api/v2/config/' + environmentName + '/tenants/' + tenantName
@@ -10,54 +8,108 @@ const headerWithApiKey = {
 
 describe('Create Tenant configuration with only root service', () => {
   it('Tenant configuration created', () => {
-    /// send Tenant configuration
-    cy.request({
-      method: 'POST',
-      url: configRequestUrl,
-      headers: headerWithApiKey,
-      body: {
-        'services' : rootOnlyConfig.services,
-        'rootServices' : rootOnlyConfig.rootServices
-      }
-    }).then((res) => {
-      expect(res.status).to.eq(201)
+
+    cy.fixture('testRootConfig').then((rootOnlyConfig) => {
+
+      /// send Tenant configuration
+      cy.request({
+        method: 'POST',
+        url: configRequestUrl,
+        headers: headerWithApiKey,
+        body: {
+          'services' : rootOnlyConfig.services,
+          'rootServices' : rootOnlyConfig.rootServices
+        }
+      }).then((res) => {
+        expect(res.status).to.eq(201)
+      })
+
+      /// Environments view
+      cy.visit('localhost:3000')
+      cy.get('[data-test="env-view-accordion"]').contains(environmentName)
+      cy.get('[data-test="env-view-tenant"]').contains(tenantName)
+      cy.get('[data-test="env-view-tenant"]').contains(rootOnlyConfig.services[0].displayName)
+        .click()
+
+      /// Service view
+      const rootServiceName = rootOnlyConfig.services[0].name
+      const rootServicePath = '/' + environmentName +
+        '/tenants/' + tenantName +
+        '/services/' + rootServiceName
+
+      cy.location('pathname').should('eq', rootServicePath)
+      cy.get('[data-test="breadcrumbs"]').contains(environmentName)
+      cy.get('[data-test="breadcrumbs"]').contains(tenantName)
+      cy.get('[data-test="breadcrumbs"]').contains(rootServiceName)
+
+      cy.contains('Status History')
+
+      cy.contains('Health Checks')
+      cy.get('[data-test="health-check-name"]')
+        .contains(rootOnlyConfig.services[0].healthChecks[0].name)
+
+      cy.contains('Services').should('not.exist')
+
+      /// cleanup - delete created tenant configuration, navigate back home
+      cy.request({
+        method: 'DELETE',
+        url: configRequestUrl,
+        headers : headerWithApiKey
+      }).then((res) => {
+        expect(res.status).to.eq(204)
+        cy.get('[data-test="navbar-home-link"]').click()
+        cy.location('pathname').should('eq', '/')
+      })
     })
 
-    /// Environments view
-    cy.visit('localhost:3000')
-    cy.get('[data-test="env-view-accordion"]').contains(environmentName)
-    cy.get('[data-test="env-view-tenant"]').contains(tenantName)
-    cy.get('[data-test="env-view-tenant"]').contains(rootOnlyConfig.services[0].displayName)
-      .click()
-
-    /// Service view
-    const rootServiceName = rootOnlyConfig.services[0].name
-    const rootServicePath = '/' + environmentName +
-                            '/tenants/' + tenantName +
-                            '/services/' + rootServiceName
-
-    cy.location('pathname').should('eq', rootServicePath)
-    cy.get('[data-test="breadcrumbs"]').contains(environmentName)
-    cy.get('[data-test="breadcrumbs"]').contains(tenantName)
-    cy.get('[data-test="breadcrumbs"]').contains(rootServiceName)
-
-    cy.contains('Status History')
-
-    cy.contains('Health Checks')
-    cy.get('[data-test="health-check-name"]')
-      .contains(rootOnlyConfig.services[0].healthChecks[0].name)
-
-    cy.contains('Services').should('not.exist')
-
-    /// cleanup - delete created tenant configuration, navigate back home
-    cy.request({
-      method: 'DELETE',
-      url: configRequestUrl,
-      headers : headerWithApiKey
-    }).then((res) => {
-      expect(res.status).to.eq(204)
-      cy.get('[data-test="navbar-home-link"]').click()
-      cy.location('pathname').should('eq', '/')
-    })
+    // /// send Tenant configuration
+    // cy.request({
+    //   method: 'POST',
+    //   url: configRequestUrl,
+    //   headers: headerWithApiKey,
+    //   body: {
+    //     'services' : rootOnlyConfig.services,
+    //     'rootServices' : rootOnlyConfig.rootServices
+    //   }
+    // }).then((res) => {
+    //   expect(res.status).to.eq(201)
+    // })
+    //
+    // /// Environments view
+    // cy.visit('localhost:3000')
+    // cy.get('[data-test="env-view-accordion"]').contains(environmentName)
+    // cy.get('[data-test="env-view-tenant"]').contains(tenantName)
+    // cy.get('[data-test="env-view-tenant"]').contains(rootOnlyConfig.services[0].displayName)
+    //   .click()
+    //
+    // /// Service view
+    // const rootServiceName = rootOnlyConfig.services[0].name
+    // const rootServicePath = '/' + environmentName +
+    //                         '/tenants/' + tenantName +
+    //                         '/services/' + rootServiceName
+    //
+    // cy.location('pathname').should('eq', rootServicePath)
+    // cy.get('[data-test="breadcrumbs"]').contains(environmentName)
+    // cy.get('[data-test="breadcrumbs"]').contains(tenantName)
+    // cy.get('[data-test="breadcrumbs"]').contains(rootServiceName)
+    //
+    // cy.contains('Status History')
+    //
+    // cy.contains('Health Checks')
+    // cy.get('[data-test="health-check-name"]')
+    //   .contains(rootOnlyConfig.services[0].healthChecks[0].name)
+    //
+    // cy.contains('Services').should('not.exist')
+    //
+    // /// cleanup - delete created tenant configuration, navigate back home
+    // cy.request({
+    //   method: 'DELETE',
+    //   url: configRequestUrl,
+    //   headers : headerWithApiKey
+    // }).then((res) => {
+    //   expect(res.status).to.eq(204)
+    //   cy.get('[data-test="navbar-home-link"]').click()
+    //   cy.location('pathname').should('eq', '/')
+    // })
   })
 })
