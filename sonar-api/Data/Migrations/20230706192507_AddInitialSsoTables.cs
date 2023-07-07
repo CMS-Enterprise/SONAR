2eeee336bf21f4f2fe6eabfd681014b2263b2b19
Stephen Brey <stephen.brey@cms.hhs.gov>
2023-07-07 16:30:47 +0000

    BATAPI-308, BATAPI-304: Adds database migrations for adding the SSO user and user_permission tables.

---
+++ sonar-api/Data/Migrations/20230706192507_AddInitialSsoTables.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cms.BatCave.Sonar.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInitialSsoTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "user",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false, collation: "ci_collation"),
+                    first_name = table.Column<string>(type: "text", nullable: false, collation: "ci_collation"),
+                    last_name = table.Column<string>(type: "text", nullable: false, collation: "ci_collation")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_permission",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    environment_id = table.Column<Guid>(type: "uuid", nullable: true),
+                    tenant_id = table.Column<Guid>(type: "uuid", nullable: true),
+                    permission = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user_permission", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_user_permission_environment_environment_id",
+                        column: x => x.environment_id,
+                        principalTable: "environment",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "fk_user_permission_tenant_tenant_id",
+                        column: x => x.tenant_id,
+                        principalTable: "tenant",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "fk_user_permission_user_user_id",
+                        column: x => x.user_id,
+                        principalTable: "user",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_email",
+                table: "user",
+                column: "email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_permission_environment_id",
+                table: "user_permission",
+                column: "environment_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_permission_tenant_id",
+                table: "user_permission",
+                column: "tenant_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_permission_user_id",
+                table: "user_permission",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user_permission");
+
+            migrationBuilder.DropTable(
+                name: "user");
+        }
+    }
+}
