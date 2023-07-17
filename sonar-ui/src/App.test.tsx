f3cd73a35706bbbae1ef86f4f558e2d18e19dde7
Stephen Brey <stephen.brey@cms.hhs.gov>
2023-07-17 23:42:15 +0000

    * Mocks config module in App.test

--- sonar-ui/src/App.test.tsx
+++ sonar-ui/src/App.test.tsx
@@ -3,6 +3,15 @@ import { MemoryRouter } from 'react-router-dom';
 import { act, render, screen } from '@testing-library/react';
 import App from './App';
 
+jest.mock('config', () => ({
+  __esModule: true,
+  apiUrl: 'https://localhost:9001',
+  oktaAuthOptions: {
+    issuer: 'https://test-mock.okta.com/oauth2/default',
+    clientId: 'test-client-id',
+    redirectUri: `http://localhost/login/callback`
+  }
+}));
 
 test('Renders Home',
   async () => {
