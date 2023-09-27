27add87cf9b6d8631bedb5aa7d18228f1c476f07
Paul Wheeler <pwheeler@revacomm.com>
2023-09-26 17:00:08 -1000

    Enable cascading delete of ErrorReport when the associated Tenant or Environment is deleted.

---
+++ sonar-api/Data/Migrations/20230927024934_ErrorReportCascadeDeletes.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cms.BatCave.Sonar.Data.Migrations {
+  /// <inheritdoc />
+  public partial class ErrorReportCascadeDeletes : Migration {
+    /// <inheritdoc />
+    /// <remarks>
+    /// Manually recreates the foreign key constraints on ErrorReport with the correct delete behavior.
+    /// </remarks>
+    protected override void Up(MigrationBuilder migrationBuilder) {
+      migrationBuilder.DropForeignKey(
+        "fk_error_report_environment_environment_id",
+        "error_report");
+
+      migrationBuilder.DropForeignKey(
+        "fk_error_report_tenant_tenant_id",
+        "error_report");
+
+      migrationBuilder.AddForeignKey(
+        name: "fk_error_report_environment_environment_id",
+        table: "error_report",
+        column: "environment_id",
+        principalTable: "environment",
+        principalColumn: "id",
+        onDelete: ReferentialAction.Cascade);
+
+      migrationBuilder.AddForeignKey(
+        name: "fk_error_report_tenant_tenant_id",
+        table: "error_report",
+        column: "tenant_id",
+        principalTable: "tenant",
+        principalColumn: "id",
+        onDelete: ReferentialAction.Cascade);
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder) {
+      migrationBuilder.DropForeignKey(
+        "fk_error_report_environment_environment_id",
+        "error_report");
+
+      migrationBuilder.DropForeignKey(
+        "fk_error_report_tenant_tenant_id",
+        "error_report");
+
+      migrationBuilder.AddForeignKey(
+        name: "fk_error_report_environment_environment_id",
+        table: "error_report",
+        column: "environment_id",
+        principalTable: "environment",
+        principalColumn: "id");
+
+      migrationBuilder.AddForeignKey(
+        name: "fk_error_report_tenant_tenant_id",
+        table: "error_report",
+        column: "tenant_id",
+        principalTable: "tenant",
+        principalColumn: "id");
+    }
+  }
+}
